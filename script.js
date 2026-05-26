59ac6127d7fa154fe8a98ea8d425c2934a857c17
planR <rplann24@gmail.com>
2026-05-26 12:39:28 +0000

    works on the JS logic

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const newBtn = document.querySelector("#new");
-const form = document.querySelector(".form");
+const form = document.querySelector("form");
 
 newBtn.addEventListener("click", () => {
-  form.classList.toggle("form");
+  form.classList.toggle("hidden");
 });
 
 
@@ -10,23 +10,44 @@ newBtn.addEventListener("click", () => {
  const myLibrary = [];
 
  function Book(title, author, page, status) {
+   this.id = crypto.randomUUID();
    this.title = title;
    this.author= author;
    this.page = page;
    this.status = status;
   }
 
-  // Book.prototype.info = function() {
-  //    return `${this.title} by ${this.author}, ${this.page} ${this.status}`;
-  // };
-
- function addBookToLibrary(hello, author, page, status) {
-  let book = new Book(hello, author, page, status);
+ function addBookToLibrary(book) {
   myLibrary.push(book);
  }
 
- addBookToLibrary("one piece", "Oda", 400, "read");
- console.log(myLibrary);
+ document.querySelector('form').addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  let book = new Book(
+    formData.get("title"),
+    formData.get("author"),
+    formData.get("pages"),
+    formData.get("status")
+  );
+  addBookToLibrary(book);
+  console.log(myLibrary);
+  e.target.reset();
+ });
+
+ function displayLibrary() {
+   for (const book of myLibrary) {
+     let tr = document.createElement("tr");
+
+     for (const [key, value] of Object.entries(book)) {
+      let td = document.createElement("td");
+      td.textContent = `${value}`;
+      tr.appendChild(td);
+      console.log(td.textContent);
+     }
+   }
+  }
+
 
 
 
